a13e7611a10e9c41878e4ed37c7b723354c4ba8d
grdn <grdn@20d0b9ba-278b-41c3-9ea2-e63b936850a0>
2012-05-23 18:38:27 +0000

    Tag the 1.7.0 release

---
+++ tags/version-1.7.0/Locales/koKR.lua
@@ -0,0 +1,4 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("iGuild", "koKR")
+if not L then return end
+
+--@localization(locale="koKR", format="lua_additive_table", handle-unlocalized="english", escape-non-ascii=true, same-key-is-true=true)@
