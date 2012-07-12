b7624ccfc06466c7567c26cb7ae19e53a5122e67
grdn <grdn@20d0b9ba-278b-41c3-9ea2-e63b936850a0>
2012-07-12 07:37:06 +0000

    - Checks if all column labels are hidden and if yes, hides the complete header line

---
+++ tags/2.0.2-beta/Developer.lua
@@ -0,0 +1,78 @@
+-----------------------------
+-- Get the addon table
+-----------------------------
+
+local AddonName, iGuild = ...;
+
+----------------------------
+-- Achievement Points
+----------------------------
+
+-- Cataclysm
+-- Patch 4.3.4 = 15250
+
+----------------------------
+
+-- Mists of Pandaria
+-- Patch 5.0.1 = 19540
+
+----------------------------
+----------------------------
+
+local achievements = {};
+
+function iGuild:CountAchievements()
+	local cats = _G.GetCategoryList();
+	
+	local numAchs = 0;
+	local numPoints = 0;
+	
+	local catName, catParent, catUnknown, catTotal, catCompleted, achID, achName, achPoints, achComplete, _, achFlags, thisID;
+	
+	for i, v in ipairs(cats) do
+		if( v == 81 ) then -- Feets of Strength
+			table.remove(cats, i);
+		end
+	end
+	
+	for i, category in ipairs(cats) do
+		catName, catParent, catUnknown = _G.GetCategoryInfo(category);
+		catTotal, catCompleted = _G.GetCategoryNumAchievements(category);
+		
+		for ach = 1, catTotal do
+			achID, achName, achPoints, achComplete, _, _, _, _, achFlags, _, _ = _G.GetAchievementInfo(category, ach);
+			thisID = achID;
+			
+			if( not achievements[achID] ) then
+				achievements[achID] = achPoints;
+				numAchs = numAchs + 1;
+				numPoints = numPoints + achPoints;
+			end
+			
+			local prev = thisID;
+			while( _G.GetPreviousAchievement(prev) ) do
+				prev = _G.GetPreviousAchievement(prev);
+				if( not achievements[prev] ) then
+					achID, achName, achPoints, achComplete, _, _, _, _, achFlags, _, _ = _G.GetAchievementInfo(prev);
+					achievements[achID] = achPoints;
+					numAchs = numAchs + 1;
+					numPoints = numPoints + achPoints;
+				end
+			end
+			
+			prev = thisID;
+			while( _G.GetNextAchievement(prev) ) do
+				prev = _G.GetNextAchievement(prev);
+				if( not achievements[prev] ) then
+					achID, achName, achPoints, achComplete, _, _, _, _, achFlags, _, _ = _G.GetAchievementInfo(prev);
+					achievements[achID] = achPoints;
+					numAchs = numAchs + 1;
+					numPoints = numPoints + achPoints;
+				end
+			end
+		end
+	end
+	
+	print("[Dev] Total Achievements: "..numAchs);
+	print("[Dev] Total Achievement Points: "..numPoints);
+end
